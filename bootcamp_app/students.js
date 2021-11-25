5043bd8da1cf354eb95ce8a8192f595c38fc6c7f
Taylor Noj <taylornoj@gmail.com>
2021-11-25 05:59:38 +0000

    update students.js to use parameterized queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,10 +1,10 @@
 const { Pool } = require('pg');
 
-// -- command line args
+// -- Command line args
 const args = process.argv.slice(2);
 console.log(args)
 
-// -- connection to database with options
+// -- Connection to database with options
 const pool = new Pool({
   user: 'vagrant',
   password: '123',
@@ -12,25 +12,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-// -- querying the database
-pool.query(`
-SELECT id, name, cohort_id
-FROM students
-LIMIT 5;
-`)
-.then(res => {  // no longer dealing with SQL, just dealing with JavaScript
-  console.log(res.rows);
-})
-.catch(err => console.error('query error', err.stack));
-// result is just an array of JavaScript Objects
+// Store user terminal input into variables
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
